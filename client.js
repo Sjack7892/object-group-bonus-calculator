b9bd0011d8b3ed155a5830e41edebd3fa1c295a6
Steve Jackson <sjack7892@gmail.com>
2020-04-07 16:57:54 -0500

    Created bonusCalculator function

--- client.js
+++ client.js
@@ -40,4 +40,81 @@ const employees = [
 // This is not a race. Everyone on your team should understand what is happening.
 // Ask questions when you don't.
 
-console.log( employees );
+console.log(employees);
+
+function employeeBonus(person) {
+  let newObject = {
+    name: person.name,
+    bonusPercentage: 5,
+    totalCompensation: (person.annualSalary),
+    totalBonus: 1,
+  };
+  // console.log(newObject)
+
+  // check a persons rating
+
+  // check a persons id
+
+  // check 
+
+  //return
+}
+console.log(employees[4].name);
+// console.log(employeeBonus(employees[4]));
+
+let array = [];
+
+function bonusCalculator() {
+  let bonPercentage;
+  for (let employee of employees) {
+    console.log(employee)  
+    if (employee.reviewRating < 3) {
+      bonPercentage = 0;
+      console.log('Bonus:', bonPercentage);
+      array.push({bonusPercentage: bonPercentage});
+    } else if (employee.reviewRating === 3) {
+      bonPercentage = 0.04;
+      console.log('Bonus:', bonPercentage);
+    } else if (employee.reviewRating === 4) {
+      bonPercentage = 0.06;
+      console.log('Bonus:', bonPercentage);
+    } else if (employee.reviewRating === 5) {
+      bonPercentage = 0.1;
+      console.log('Bonus:', bonPercentage);
+    }
+    if (employee.employeeNumber < 10000) {
+      bonPercentage += 0.05;
+      console.log('Bonus plus tenure:',bonPercentage);
+    } 
+    if (employee.annualSalary > 65000) {
+      bonPercentage -= 0.01;
+      console.log('Bonus after reduction:',bonPercentage);
+    } 
+    if (bonPercentage > 0.13) {
+        bonPercentage = 0.13;
+        console.log('Final Bonus:', bonPercentage);
+    } else if (bonPercentage < 0) {
+       bonPercentusage = 0;
+       console.log('Final Bonus:', bonPercentage);
+    }
+  }
+}
+bonusCalculator();
+
+
+// function checkPenguins(allPenguins, maxHeight) {
+//     let allMatchingPenguins = [];
+//     // penguin is the same as saying allPenguins[i] in a traditional for loop
+//     for (let penguin of allPenguins) {
+//         console.log(penguin);
+//         if (penguin.isCute && penguin.size < maxHeight) {
+//             console.log(`It's cute!`)
+//             console.log(`It's smaller!`)
+//             //add current penguin into allMatchingPenguins
+//             allMatchingPenguins.push(penguin)
+//         }
+//     }
+//     return allMatchingPenguins;
+// } // end function checkPenguins
+
+
